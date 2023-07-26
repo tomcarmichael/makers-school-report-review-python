cedf3dba94316678c0b4227073103d4fc60cd14a
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-07-26 16:40:01 +0100

    Refactored code to improve readability

--- school_report.py
+++ school_report.py
@@ -5,58 +5,27 @@ def school_report(results_string):
     if len(results) == 1:
         results = results[0].split(',')
 
-    output = ''
-
     grades = ('Green', 'Amber', 'Red')
 
     result_counts = {}
-
+    # Add each grade to the results_counts with a starting count of 0
     for grade in grades:
         result_counts[grade] = 0
     
     for result in results:
-        formatted_result = result.capitalize()
+        result = result.capitalize()
 
-        if formatted_result in result_counts.keys():
-                result_counts[formatted_result] += 1
+        if result in grades:
+            result_counts[result] += 1
         else:
             if 'Uncounted' in result_counts.keys():
                 result_counts['Uncounted'] += 1
             else:
                 result_counts['Uncounted'] = 1
-    
+
+    output = ''
     for key in result_counts:
         if result_counts[key] > 0:
             output += f'{key}: {result_counts[key]}\n'
     
-    return output.strip()
-
-
-    # results_list = results_string.split(',')
-
-    # grades = ('Green', 'Amber', 'Red')
-
-    # result_counts = {}
-
-    # for grade in grades:
-    #     result_counts[grade] = 0
-    
-    # result_counts['Uncounted'] = 0
-
-    # for result in results_list:
-    #     for grade in grades:
-    #         if result.upper() == grade.upper():
-    #             result_counts[grade] += 1
-    #         else:
-    #             result_counts['Uncounted'] += 1
-
-    # output = f''
-
-    # for key in result_counts:
-    #     if result_counts[key] > 0:
-    #         output += f'{key}: {result_counts[key]}\n'
-    
-    # return output
-        
-    
-
+    return output.strip()  
