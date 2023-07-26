9098cdf40d6f7d6d97a09b360da25a2b5f3062e3
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-07-26 13:40:01 +0100

    Implement 2 passing tests with explicit return and if else statement given only 1 result

---
+++ school_report.py
@@ -0,0 +1,5 @@
+def school_report(results_string):
+    if results_string == 'Green':
+        return 'Green: 1'
+    elif results_string == 'Red':
+        return 'Red: 1'
---
+++ test_school_report.py
@@ -0,0 +1,9 @@
+from school_report import school_report 
+
+def test_school_report_result_1():
+    input = "Green"
+    assert(school_report(input)) == 'Green: 1'
+
+def test_school_report_result_2():
+    input = "Red"
+    assert(school_report(input)) == 'Red: 1'
